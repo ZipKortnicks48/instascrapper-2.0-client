1b45e751ca103206f3bd7ef0d874e13fd009bebf
Александр Сырокласов <SiroklasovA@rkvv.local>
2020-01-28 16:35:46 +0300

    first commit

---
+++ appUI/app_main.py
@@ -0,0 +1,103 @@
+
+import formsUI.form_main as form_main
+from appUI.app_server_settings import AppServerSettings
+from src.formHandler import formHandler
+from PyQt5 import QtWidgets
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QMessageBox, QSystemTrayIcon,QStyle,QAction,qApp,QMenu
+from datetime import datetime
+from threading import Thread
+import schedule
+import time
+import subprocess
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+class App(QtWidgets.QMainWindow, QtWidgets.QTableWidgetItem, form_main.Ui_Dialog,QtWidgets.QErrorMessage,QtWidgets.QHeaderView):
+    def __init__(self):
+        # Это здесь нужно для доступа к переменным, методам
+        # и т.д. в файле design.py
+        super().__init__()
+        self.startEvent = True
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+        self.showCommentsButton.pressed.connect(self.showCommentsButtonClick)
+        self.buttonHand.pressed.connect(self.handSearch)
+        #иконка трея
+        self.tray_icon = QSystemTrayIcon(self)
+        self.tray_icon.setIcon(self.style().standardIcon(QStyle.SP_ComputerIcon))
+        self.tray_icon = QSystemTrayIcon(self)
+        self.tray_icon.setIcon(self.style().standardIcon(QStyle.SP_ComputerIcon))
+ 
+        '''
+            show - показать окно
+            hide - скрыть окно
+            exit - выход из программы
+        '''
+        show_action = QAction("Развернуть", self)
+        quit_action = QAction("Выход", self)
+        hide_action = QAction("Скрыть", self)
+        show_action.triggered.connect(self.show)
+        hide_action.triggered.connect(self.hide)
+        quit_action.triggered.connect(qApp.quit)
+        tray_menu = QMenu()
+        tray_menu.addAction(show_action)
+        tray_menu.addAction(hide_action)
+        tray_menu.addAction(quit_action)
+        self.tray_icon.setContextMenu(tray_menu)
+        self.tray_icon.show()
+        self.newServerSettingsItem.triggered.connect(self.showNewServerSettingsWindow)
+    def showNewServerSettingsWindow(self):
+        self.windowServer = AppServerSettings()  # Создаём объект класса ExampleApp
+        self.windowServer.show()  # Показываем окно
+    #ручной поиск по словам 
+    def handSearch(self):
+        word=self.textBoxSearch.text()
+        username=self.comboBoxAcc.currentText()
+        self.handler=formHandler(username)
+        self.tableComments.clear()
+        comments=self.handler.bdAPI.getCommentHandsearch(word)
+        self.tableComments.setRowCount(len(comments))
+        row=0
+        for comment in comments:
+                self.tableComments.setItem(row, 0,  QtWidgets.QTableWidgetItem(datetime.fromtimestamp(int(comment["comment_date"])).strftime("%d-%m-%Y %H:%M")))
+                self.tableComments.setItem(row, 1,  QtWidgets.QTableWidgetItem(comment["comment_text"]))
+                self.tableComments.setItem(row, 2,  QtWidgets.QTableWidgetItem(self.handler.bdAPI.getLinkByCommentId(comment["comment_id"])))
+                row += 1
+        self.setHeaderTittle()
+        QMessageBox.about(self,"Уведомление","Поиск завершен")
+    def enableButtons(self):
+        self.autoButton.setEnabled(True)
+        self.findNewComButton.setEnabled(True)
+    #отобразить найденные комменты по словарю
+    def showCommentsButtonClick(self):
+        username=self.comboBoxAcc.currentText()
+        self.handler=formHandler(username)
+        self.tableComments.clear()
+        comments=self.handler.bdAPI.showGoodComments()
+        self.tableComments.setRowCount(len(comments))
+        row=0
+        for comment in comments:
+                self.tableComments.setItem(row, 0,  QtWidgets.QTableWidgetItem(datetime.fromtimestamp(int(comment['comment_date'])).strftime("%d-%m-%Y %H:%M")))
+                self.tableComments.setItem(row, 1,  QtWidgets.QTableWidgetItem(comment['comment_text']))
+                self.tableComments.setItem(row, 2,  QtWidgets.QTableWidgetItem(self.handler.bdAPI.getLinkByCommentId(comment['comment_id'])))
+                row += 1
+        self.setHeaderTittle()
+        QMessageBox.about(self,"Уведомление","Поиск завершен")
+    #поиск по новому словарю
+    def searchButtonClick(self):
+        username=self.comboBoxAcc.currentText()
+        self.handler=formHandler(username)
+        #QMessageBox.about(self,"Уведомление","Ищем по новому словарю") 
+        self.handler.bdAPI.checkComments()
+    #событие закрытия окна
+    def closeEvent(self, event):
+        event.ignore()
+        self.hide()
+        self.tray_icon.showMessage(
+            "Программа свернута",
+            "Программа была свернута в трей.",
+            QSystemTrayIcon.Information,
+            2000
+            )
+    def setHeaderTittle(self):
+        self.tableComments.setHorizontalHeaderLabels(['Дата', 'Текст', 'Ссылка на запись'])
---
+++ formsUI/form_main.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainform.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.setFixedSize(629, 638)
+        self.textBoxSearch = QtWidgets.QLineEdit(Dialog)
+        self.textBoxSearch.setGeometry(QtCore.QRect(390, 61, 211, 21))
+        self.textBoxSearch.setObjectName("textBoxSearch")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(460, 40, 181, 16))
+        self.label.setObjectName("label")
+        self.comboBoxAcc = QtWidgets.QComboBox(Dialog)
+        self.comboBoxAcc.setGeometry(QtCore.QRect(20, 60, 141, 22))
+        self.comboBoxAcc.setObjectName("comboBoxAcc")
+        self.comboBoxAcc.addItem("")
+        self.comboBoxAcc.addItem("")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(20, 30, 201, 20))
+        self.label_2.setObjectName("label_2")
+        self.tableComments = QtWidgets.QTableWidget(Dialog)
+        self.tableComments.setSizeAdjustPolicy(
+        QtWidgets.QAbstractScrollArea.AdjustToContents)
+        self.tableComments.setGeometry(QtCore.QRect(20, 90, 581, 471))
+        self.tableComments.setObjectName("tableComments")
+        self.tableComments.setColumnCount(3)
+        self.tableComments.setRowCount(0)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableComments.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableComments.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableComments.setHorizontalHeaderItem(2, item)
+        self.buttonHand = QtWidgets.QPushButton(Dialog)
+        self.buttonHand.setGeometry(QtCore.QRect(350, 60, 41, 23))
+        self.buttonHand.setObjectName("buttonHand")
+        self.showCommentsButton = QtWidgets.QPushButton(Dialog)
+        self.showCommentsButton.setGeometry(QtCore.QRect(314, 60, 41, 23))
+        self.showCommentsButton.setObjectName("showCommentsButton")
+        menubar = self.menuBar()
+        self.fileMenu = menubar.addMenu('Опции')
+        self.newServerSettingsItem=self.fileMenu.addAction("Настройки сервера")
+        self.helpMenu=menubar.addMenu("Помощь")
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Instascrapper 2.0 Client"))
+        self.label.setText(_translate("Dialog", "Введите слово для поиска"))
+        self.comboBoxAcc.setItemText(0, _translate("Dialog", "igor_artamonov48"))
+        self.comboBoxAcc.setItemText(1, _translate("Dialog", "igor_artamonov48"))
+        self.label_2.setText(_translate("Dialog", "Выберите аккаунт для отслеживания"))
+        item = self.tableComments.horizontalHeaderItem(0)
+        item.setText(_translate("Dialog", "Дата"))
+        item = self.tableComments.horizontalHeaderItem(1)
+        item.setText(_translate("Dialog", "Текст"))
+        self.tableComments.setColumnWidth(1, 260)
+        self.tableComments.horizontalHeader().setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)
+        item = self.tableComments.horizontalHeaderItem(2)
+        item.setText(_translate("Dialog", "Ссылка на запись"))
+        self.buttonHand.setText(_translate("Dialog", "🔍"))
+        self.showCommentsButton.setText(_translate("Dialog", "🕮"))
